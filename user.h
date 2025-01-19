b18ee13e67d01bbf271d9ee808164900a0a43567
Asnd123 <kuba.klyszczynski@o2.pl>
2025-01-19 15:39:37 +0100

    Update user.h

--- user.h
+++ user.h
@@ -1,1 +1,24 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
 
+class User {
+private:
+    string imie;
+    string nazwisko;
+    string haslo;
+    string login;
+    vector<string> historiaWypozyczen;
+
+public:
+    User(string Imie, string Nazwisko, string Login, string Haslo);
+    void setHaslo(string Haslo);
+    void setLogin(string Login);
+    void setNazwisko(string Nazwisko);
+    void setImie(string Imie);
+    bool sprawdzDane(string wpisanyLogin, string wpisaneHaslo) const;
+    void pokazDane() const;
+    void dodajDoHistorii(const string& tytul);
+    void pokazHistorie() const;
+};
